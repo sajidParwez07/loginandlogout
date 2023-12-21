ca617485de4a88f0fb15dbc0770d1c8cc7f971ea
sajidParwez07 <136393992+sajidParwez07@users.noreply.github.com>
2023-12-22 02:05:25 +0530

    delete some code in cookies

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -27,15 +27,11 @@ const Login = async (req, res) => {
             const token = await user.generateAuthToken();
             res.cookie('jwtoken', token, {
                 expires: new Date(Date.now() + 25892000000),
-                httpOnly: false,
-                secure: true,
-                sameSite: 'None'
+                secure: true    
             });
             res.cookie('userData', user._id, {
                 expires: new Date(Date.now() + 25892000000),
-                httpOnly: true,
-                secure: true,
-                sameSite: 'None'
+                secure: true
             });
             res.status(200).json({ message: 'user login successfully...!!!' });
         }
